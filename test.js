fc3823e5fc4394ed3c9622650477115e1dad830e
Lucas Wiener <lucas@wiener.se>
2016-02-05 15:32:02 +0100

    Added more tests

--- test.js
+++ test.js
@@ -27,4 +27,90 @@ function test(expected, actual) {
     bp.force();
 
     test("00112", result);
-})()
+})();
+
+(function async() {
+    var bp = BatchProcessor({
+        auto: false,
+        async: true
+    });
+
+    var result = "";
+
+    function f(v) {
+        result += v;
+    }
+
+    bp.add(0, f.bind(null, "0"));
+    bp.add(0, f.bind(null, "0"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(2, f.bind(null, "2"));
+
+    bp.force();
+
+    test("", result);
+
+    setTimeout(function () {
+        test("00112", result);
+    }, 10);
+})();
+
+(function autoAsync() {
+    var bp = BatchProcessor({
+        auto: true,
+        async: true
+    });
+
+    var result = "";
+
+    function f(v) {
+        result += v;
+    }
+
+    bp.add(0, f.bind(null, "0"));
+    bp.add(0, f.bind(null, "0"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(2, f.bind(null, "2"));
+
+    test("", result);
+
+    setTimeout(function () {
+        test("00112", result);
+    }, 10);
+})();
+
+(function autoSync() {
+    var warn = "";
+
+    var bp = BatchProcessor({
+        auto: true,
+        async: false,
+        reporter: {
+            warn: function () {
+                warn = "called";
+            }
+        }
+    });
+
+    test("called", warn);
+
+    var result = "";
+
+    function f(v) {
+        result += v;
+    }
+
+    bp.add(0, f.bind(null, "0"));
+    bp.add(0, f.bind(null, "0"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(1, f.bind(null, "1"));
+    bp.add(2, f.bind(null, "2"));
+
+    test("", result);
+
+    setTimeout(function () {
+        test("00112", result);
+    }, 10);
+})();
